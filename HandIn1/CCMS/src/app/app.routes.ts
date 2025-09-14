dfc9e26f6a158c3864c59c5c90efb11850a10d05
lassebp7 <lasseborring@hotmail.com>
2025-09-14 19:51:35 +0200

    Added lazy loaded route

--- HandIn1/CCMS/src/app/app.routes.ts
+++ HandIn1/CCMS/src/app/app.routes.ts
@@ -1,9 +1,22 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AddCreditCardComponent } from './pages/AddCreditCard/AddCreditCard.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'add',
+    component: AddCreditCardComponent,
+  },
+  // Lazy loaded route
+  {
+    path: 'transactions',
+    loadComponent: () =>
+      import('./pages/Transactions/Transactions.component').then(
+        (m) => m.TransactionsComponent
+      ),
   },
 ];
