9d714d8a55815070c1817ede109bb537e34b2b32
lassebp7 <lasseborring@hotmail.com>
2025-09-14 21:47:06 +0200

    Basic auth

--- HandIn1/CCMS/src/app/pages/home/home.component.ts
+++ HandIn1/CCMS/src/app/pages/home/home.component.ts
@@ -1,4 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +13,23 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './home.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent { }
+export class HomeComponent {
+  private authService = inject(AuthService);
+  public username = environment.username;
+  public password = environment.password;
+
+  login() {
+    this.authService.login(this.username, this.password).subscribe({
+      next: (token) => {
+        localStorage.setItem('token', token);
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+}
---
+++ HandIn1/CCMS/src/app/services/auth.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+interface Login {
+  username: string;
+  password: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private http = inject(HttpClient);
+  private baseUrl = environment.baseUrl;
+  login(username: string, password: string) {
+    const loginBody: Login = {
+      username,
+      password,
+    };
+    console.log(loginBody);
+    return this.http.post(`${this.baseUrl}/api/Login`, loginBody, {
+      responseType: 'text',
+    });
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+  }
+}
