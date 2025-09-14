154edf469672d75c0fda78e12ed71f01008d136d
lassebp7 <lasseborring@hotmail.com>
2025-09-14 19:36:29 +0200

    skeleton with navbar

---
+++ HandIn1/CCMS/src/app/pages/AddCreditCard/AddCreditCard.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-credit-card',
+  imports: [],
+  templateUrl: './AddCreditCard.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AddCreditCardComponent { }
---
+++ HandIn1/CCMS/src/app/pages/Transactions/Transactions.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transactions',
+  imports: [],
+  templateUrl: './Transactions.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TransactionsComponent { }
