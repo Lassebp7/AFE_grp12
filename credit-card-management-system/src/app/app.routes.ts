9b737a27cae887d95a3dcf2138c3fb6c415b2719
Esben Inglev <esbeninglev@gmail.com>
2025-09-18 12:50:19 +0200

    added auth guard

--- credit-card-management-system/src/app/app.routes.ts
+++ credit-card-management-system/src/app/app.routes.ts
@@ -3,22 +3,26 @@ import {Home} from './home/home';
 import {AddCredit} from './add-credit/add-credit';
 import {Transactions} from './transactions/transactions';
 import {NotFound} from './not-found/not-found';
+import {authGuard} from './guards/auth-guard';
 
 export const routes: Routes = [
   {
     title: "Home",
     path: "",
     component: Home,
+    canActivateChild: [authGuard]
   },
   {
     title: "Add Credit",
     path: "add-credit",
-    component: AddCredit
+    component: AddCredit,
+    canActivateChild: [authGuard]
   },
   {
     title: "Transactions",
     path: "transactions",
-    component: Transactions
+    component: Transactions,
+    canActivateChild: [authGuard]
   },
   {
     path: "**",
