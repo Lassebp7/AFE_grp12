ed73a9dddbd17146a4d775965cf9f6f5a4e349af
Esben Inglev <esbeninglev@gmail.com>
2025-09-18 15:10:19 +0200

    added exp pipe

--- credit-card-management-system/src/app/home/credit-card-list/credit-card-item/credit-card-item/credit-card-details.component.ts
+++ credit-card-management-system/src/app/home/credit-card-list/credit-card-item/credit-card-item/credit-card-details.component.ts
@@ -1,12 +1,14 @@
 import { Component, input } from '@angular/core';
 import { CreditCard } from '../../../../models/credit-card';
+import {ExpirationPipe} from '../../../../pipes/expiration-pipe';
 
 @Component({
   selector: 'app-credit-card-details',
-  imports: [],
+  imports: [ExpirationPipe],
   templateUrl: './credit-card-details.component.html',
 })
 export class CreditCardDetails {
   public creditCardInfo = input.required<CreditCard>();
   public transactions = input.required<Transaction[]>();
+  protected readonly ExpirationPipe = ExpirationPipe;
 }
